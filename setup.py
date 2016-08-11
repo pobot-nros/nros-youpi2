dc56373cd22793efcd9d2b0bffa9ce7bb8c2e69d
Eric Pascual <eric@pobot.org>
2016-08-11 23:25:43 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nros-youpi2',
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+        'write_to': 'src/nros/youpi2/__version__.py'
+    },
+    namespace_packages=['pybot'],
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    install_requires=['nros-core', 'pybot-youpi2'],
+    download_url='https://github.com/Pobot/PyBot',
+    description='Youpi2 arm nROS node',
+    entry_points={
+        'console_scripts': [
+            'start-youpi2-node = nros.youpi2.entry_points:start_node'
+        ]
+    }
+)
