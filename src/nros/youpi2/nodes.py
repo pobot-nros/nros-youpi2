16106c5cf2788fc26b098b08b1b651dcc3e696d4
Eric Pascual <eric@pobot.org>
2016-09-10 12:08:24 +0200

    fixed wrong published methods signature

--- src/nros/youpi2/nodes.py
+++ src/nros/youpi2/nodes.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
 import dbus.service
 
 from nros.core.node import NROSNode, DEFAULT_SERVICE_OBJECT_PATH
-from pybot.youpi2.arm import YoupiArm
+from pybot.youpi2.model import YoupiArm
 
 __author__ = 'Eric Pascual'
 
@@ -58,35 +56,35 @@ class ArmServiceObject(dbus.service.Object):
             self._logger.info('service object created at path "%s"', svc_obj_path)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='b')
-    def open_gripper(self, wait=True):
+    def open_gripper(self, wait):
         self._arm.open_gripper(wait)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='b')
-    def close_gripper(self, wait=True):
+    def close_gripper(self, wait):
         self._arm.close_gripper(wait)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='b')
-    def calibrate_gripper(self, wait=True):
+    def calibrate_gripper(self, wait):
         self._arm.calibrate_gripper(wait)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='ai')
-    def seek_origins(self, joint_sequence=None):
+    def seek_origins(self, joint_sequence):
         self._arm.seek_origins(joint_sequence)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='ib')
-    def rotate_hand(self, angle, wait=True):
+    def rotate_hand(self, angle, wait):
         self._arm.rotate_hand(angle, wait)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='ib')
-    def rotate_hand_to(self, angle, wait=True):
+    def rotate_hand_to(self, angle, wait):
         self._arm.rotate_hand_to(angle, wait)
 
-    @dbus.service.method(INTERFACE_NAME, in_signature='a{ii}b')
-    def move(self, angles, wait=True):
+    @dbus.service.method(INTERFACE_NAME, in_signature='a{id}b')
+    def move(self, angles, wait):
         self._arm.coupled_joints_move(angles, wait)
 
-    @dbus.service.method(INTERFACE_NAME, in_signature='a{ii}b')
-    def goto(self, angles, wait=True):
+    @dbus.service.method(INTERFACE_NAME, in_signature='a{id}b')
+    def goto(self, angles, wait):
         self._arm.coupled_joints_goto(angles, wait)
 
     @dbus.service.method(INTERFACE_NAME, in_signature='b')
@@ -101,6 +99,10 @@ class ArmServiceObject(dbus.service.Object):
     def hard_hi_Z(self):
         self._arm.hard_hi_Z()
 
+    @dbus.service.method(INTERFACE_NAME, out_signature='ad')
+    def get_current_positions(self):
+        return self._arm.get_joint_positions()
+
 
 def start_node():
-    ArmNode.main(sys.argv)
+    ArmNode.main()
