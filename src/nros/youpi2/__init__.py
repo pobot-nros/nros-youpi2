7cf4265bdaffd8e17ad1f04d13b4e83f46fa8b4b
Eric Pascual <eric@pobot.org>
2016-09-10 18:25:50 +0200

    promote definitions to package level

---
+++ src/nros/youpi2/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from nros.core.node import DEFAULT_SERVICE_OBJECT_PATH
+
+SERVICE_OBJECT_PATH = DEFAULT_SERVICE_OBJECT_PATH
+ARM_CONTROL_INTERFACE_NAME = 'org.pobot.nros.youpi'
